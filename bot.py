b8cda46fa996035c5558ba5c2837547e79f921a2
APPALACHIANIN <71959685+APPALACHIANIN@users.noreply.github.com>
2020-09-29 19:06:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands 
 import os
+import datetime
+
 
 
 PREFIX = '$'
@@ -11,7 +13,7 @@ client.remove_command( 'help' )
 @client.event
 
 async def on_ready():
-	print( 'BOT connected' )
+	print( 'Бот подключился к серверу' )
 
 # Clear message
 @client.command ( pass_context = True )
@@ -35,10 +37,16 @@ async def kick( ctx, member: discord.Member, *, reason = None ):
 @commands.has_permissions( administrator = True )
 
 async def ban( ctx, member: discord.Member, *, reason = None ):
+	emb = discord.Embed( title = 'Блокировка участника', colour = discord.Color.red() )
 	await ctx.channel.purge( limit = 1 )
 
 	await member.ban( reason = reason )
-	await ctx.send( f'Пользователь { member.mention } был забанен за нарушение правил сервера' )
+
+	emb.set_author( name = member.name, icon_url = member.avatar_url )
+	emb.add_field( name = 'Бан' , value = 'Пользователь заблокирован : {}'.format( member.mention ) )
+	emb.set_footer( text = 'Был забанен администратором {}'.format( ctx.author.name ), icon_url = ctx.author.avatar_url )
+
+	await ctx.send( embed = emb )
 
 # Unban
 @client.command( pass_context = True )
@@ -62,30 +70,43 @@ async def unban( ctx, *, member ):
 @commands.has_permissions( administrator = True ) 
 
 async def help( ctx ):
-	emb = discord.Embed( title = 'Навигация по командам сервера')
+	emb = discord.Embed( title = 'Навигация по командам сервера', colour = discord.Color.orange() )
 
 	emb.add_field( name = '{}clear'.format( PREFIX ), value = 'Очистка чата на n-количество сообщений' )
 	emb.add_field( name = '{}kick'.format( PREFIX ), value = 'Дисквалификация участника с сервера' )
 	emb.add_field( name = '{}ban'.format( PREFIX ), value = 'Ограничение участнику доступа к серверу' )
 	emb.add_field( name = '{}unban'.format( PREFIX ), value = 'Возобновление участнику доступа к серверу' )
+	emb.add_field( name = '{}time'.format( PREFIX ), value = 'Время по Иркутску' )
 
 	await ctx.send( embed = emb )
 
-@client.command( pass_context = True )
-@commands.has_permissions( administrator = True )
+# Time
 
 async def time( ctx ):
-	emb = discord.Embed( title = 'Твой заголовок', colour = discord.Color.green(), url = 'https://www.timeserver.ru/cities/kz/taldykorgan' )
+	emb = discord.Embed( title = 'Ваше время', description = 'Здесь вы сможете узнать время по городу' , colour = discord.Color.green(), url = 'https://www.timeserver.ru/cities/kz/taldykorgan' )
 
 	emb.set_author( name = client.user.name, icon_url = client.user.avatar_url )
-	emb.set_footer( text = ctx.author.name, icon_url = ctx.author.avatar_url )
-	emb.set_image( url = 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/applewatch/watchos6-series5-watch-not-connected.jpg' )
-	emb.thumbnail( url = 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/applewatch/watchos6-series5-watch-not-connected.jpg' )
+	emb.set_footer( text = 'Иди своей дорогой, сталкер' , icon_url = ctx.author.avatar_url )
+	emb.set_image( url = 'http://pics.wikireality.ru/upload/e/ed/Villi-vonka-nastalo-vremya.jpeg' )
+	emb.set_thumbnail( url = 'https://soundtimes.ru/images/simfjniya/610.jpg' )
 
 	now_date = datetime.datetime.now()
-	emb.add_field( name = 'Time', value = 'Время : {}'.format( now_date ) )
+
+	emb.add_field( name = 'Время', value = 'То самое время : {}'.format( now_date ) )
 
 	await ctx.send( embed = emb )
+
+# Mute
+@client.command()
+@commands.has_permissions( administrator = True )
+
+async def mute( ctx, member: discord.Member ):
+	await ctx.channel.purge( limit = 1 )
+
+	mute_role = discord.utils.get( ctx.message.guild.roles, name = 'MUTE' )
+
+	await member.add_roles( mute_role )
+	await ctx.send( f'У { member.mention }, ограничение чата за нарушение прав!' )
 
 # Get token
 token = os.environ.get('BOT_TOKEN')
